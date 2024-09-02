c87587346e06cd72633203272e4b8fe599f668b6
Richy Osano <richardosano97@gmail.com>
2024-09-02 17:45:51 +0300

    Rendered each component in colors array

--- src/01-color-renderer/ColorRenderer.js
+++ src/01-color-renderer/ColorRenderer.js
@@ -1,27 +1,24 @@
-import Color from './Color'
+import Color from './Color';
 
-const colors = [{
-  hex: '#91A6FF',
-  name: 'Cornflower Blue'
-},
-{
-  hex: '#FF88DC',
-  name: 'Persian Pink'
-},
-{
-  hex: '#80FF72',
-  name: 'Screamin Green'
-},
-{
-  hex: '#FF5154',
-  name: 'Tart Orange'
-}]
+const colors = [
+	{
+		hex: '#91A6FF',
+		name: 'Cornflower Blue',
+	},
+	{
+		hex: '#FF88DC',
+		name: 'Persian Pink',
+	},
+	{
+		hex: '#80FF72',
+		name: 'Screamin Green',
+	},
+	{
+		hex: '#FF5154',
+		name: 'Tart Orange',
+	},
+];
 
-export default function ColorRenderer () {
-  return (
-    <h2>
-      Use the Color component to render each
-       item in the colors array on the page!
-    </h2>
-  )
+export default function ColorRenderer() {
+	return colors.map(({ hex, name }) => <Color key={hex} hex={hex} name={name} />);
 }
