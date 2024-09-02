9507edb3bdf3877e7d32794ffd321dfa21e65c47
Richy Osano <richardosano97@gmail.com>
2024-09-02 20:46:04 +0300

    Added validation to form

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,29 +1,54 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-export default function FormValidator () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+export default function FormValidator() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [passwordConfirm, setPasswordConfirm] = useState('');
+	const [validationMessage, setValidationMessage] = useState('');
 
-  return (
-    <form>
-      <h2>Sign Up!</h2>
-      <label htmlFor='email'>Email</label>
-      <input
-        type='text' name='email'
-        onChange={e => setEmail(e.target.value)}
-      />
-      <label htmlFor='password'>Password</label>
-      <input
-        type='password' name='password'
-        onChange={e => setPassword(e.target.value)}
-      />
-      <label htmlFor='password-confirm'>Confirm Password </label>
-      <input
-        type='password' name='password-confirm'
-        onChange={e => setPasswordConfirm(e.target.value)}
-      />
-      <input type='submit' value='Submit' />
-    </form>
-  )
+	const findErrors = () => {
+		const errors = [];
+		if (!email || !password || !passwordConfirm) {
+			errors.push('All items must contain a value');
+		}
+		if ([...email].filter((chr) => chr === '@').length !== 1) {
+			errors.push('An email must have exactly one @ sign');
+		}
+		if (password.length < 8) {
+			errors.push('Passwords must be 8 characters or longer');
+		}
+		if (password !== passwordConfirm) {
+			errors.push('Passwords must match');
+		}
+
+		return errors;
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const errors = findErrors();
+		setValidationMessage(errors.length ? errors.join(', ') : 'User Created!');
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<h2>Sign Up!</h2>
+			<label htmlFor='email'>Email</label>
+			<input type='text' name='email' onChange={(e) => setEmail(e.target.value)} />
+			<label htmlFor='password'>Password</label>
+			<input
+				type='password'
+				name='password'
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<label htmlFor='password-confirm'>Confirm Password </label>
+			<input
+				type='password'
+				name='password-confirm'
+				onChange={(e) => setPasswordConfirm(e.target.value)}
+			/>
+			{validationMessage}
+			<input type='submit' value='Submit' />
+		</form>
+	);
 }
